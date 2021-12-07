0bda78c10fabda1df920fe6fc418db65d922fe61
sergioantelo <santelocabello02@gmail.com>
2021-12-07 18:03:36 +0400

    How to: Git and Github

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+//Bundle your Javascript files
+
+module.exports = {
+    entry: [
+        './src/main.js'
+    ],
+    output: {
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
